7db2e66750a09fd83a8fc4a0a3e7adc457eb3bb0
Leonidas Avdelas <avdelasleonidas@gmail.com>
2020-12-24 23:18:02 +0200

    Fixed a bug where the SpaceVim.d might not be created

--- use_spacevim.sh
+++ use_spacevim.sh
@@ -4,5 +4,6 @@ bash ./install_requirements.sh
 
 curl -sLf https://spacevim.org/install.sh | bash
 
+mkdir -p ${HOME}/.SpaceVim.d/
 rm ${HOME}/.SpaceVim.d/init.toml
 cp ./init.toml ${HOME}/.SpaceVim.d/
